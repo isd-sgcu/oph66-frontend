02a0b9e16355754bfdbdae44c810ad0dc211c9b4
PwFaze <feze2013@hotmail.com>
2023-12-27 04:09:16 +0700

    feat: searchbar and selector

--- src/components/Event/EventBox.tsx
+++ src/components/Event/EventBox.tsx
@@ -1,28 +1,48 @@
-export default function EventBox() {
-  const EnrollBox = () => {
-    return (
-      <div className="flex flex-row gap-2 text-white">
-        <div className="rounded-full bg-pink-500 px-2 py-1 ">
-          <p className="text-xs">ต้องลงทะเบียน/ Limited seats</p>
-        </div>
-        <div className="rounded-full border-2 border-white px-2 py-1">
-          <p className="text-xs">999/999</p>
-        </div>
-      </div>
-    );
-  };
+export interface EventBoxProps {
+  EventName: string;
+  Enroll: boolean;
+  CurrentEnrollment: number;
+  MaxEnrollment: number;
+  ThaiFaculty: string;
+  EngFaculty: string;
+  Date: string;
+  Time: string;
+  Location: string;
+}
+const EnrollBox: React.FC<{
+  CurrentEnrollment: number;
+  MaxEnrollment: number;
+}> = ({ CurrentEnrollment, MaxEnrollment }) => (
+  <div className="flex flex-row gap-2 text-white">
+    <div className="rounded-full bg-pink-500 px-2 py-1 ">
+      <p className="text-xs">ต้องลงทะเบียน/ Limited seats</p>
+    </div>
+    <div className="rounded-full border-2 border-white px-2 py-1">
+      <p className="text-xs">
+        {CurrentEnrollment} / {MaxEnrollment}
+      </p>
+    </div>
+  </div>
+);
+
+const EventBox = (props: EventBoxProps) => {
   return (
     <div className="box-border w-72 rounded-lg border-2 border-white bg-gradient-to-br from-gray-100 to-pink-400 shadow-md ring-offset-2 ring-offset-white">
       <a href="\">
         <div className="border-1 rounded-md border-black bg-gradient-to-r from-[#393570]  to-[#CA4072] px-4 py-2">
           <p className="font-ibm text-xl font-semibold uppercase text-white">
-            Session talk with ajarn
+            {props.EventName}
           </p>
-          <EnrollBox />
+          {props.Enroll && (
+            <EnrollBox
+              CurrentEnrollment={props.CurrentEnrollment}
+              MaxEnrollment={props.MaxEnrollment}
+            />
+          )}
         </div>
         <div className="flex flex-col px-4 py-2 text-start text-white">
-          <p>คณะสถาปัตยกรรมศาสตร์</p>
-          <p className="text-sm">Faculty of Architecture</p>
+          <p>{props.ThaiFaculty}</p>
+          <p className="text-sm">{props.EngFaculty}</p>
           <div className="flex flex-col gap-2">
             <div className="flex gap-2">
               <svg
@@ -41,7 +61,7 @@ export default function EventBox() {
                   fill="#FDFFFC"
                 />
               </svg>
-              20-21 January 2024
+              {props.Date}
             </div>
             <div className="flex gap-2">
               <svg
@@ -66,7 +86,7 @@ export default function EventBox() {
                   fill="#FDFFFC"
                 />
               </svg>
-              10.00-17.00
+              {props.Time}
             </div>
             <div className="flex gap-2">
               <svg
@@ -84,11 +104,12 @@ export default function EventBox() {
                   fill="#FDFFFC"
                 />
               </svg>
-              คณะสถาปัตยกรรมศาสตร์ / Faculty of Architecture
+              {props.Location}
             </div>
           </div>
         </div>
       </a>
     </div>
   );
-}
+};
+export default EventBox;
--- src/components/Event/EventContainer.tsx
+++ src/components/Event/EventContainer.tsx
@@ -1,16 +1,99 @@
-import EventBox from "./EventBox";
+import { useState } from "react";
+import EventBox, { type EventBoxProps } from "./EventBox";
 
-export default function EventContainer() {
+const Faculties = {
+  1: "Engineer",
+  2: "Science",
+  3: "Arts",
+};
+
+const EventContainer = ({ Events }: { Events: EventBoxProps[] }) => {
+  const [selectedFaculty, setSelectedFaculty] = useState<string | null>(null);
+  const [currEvents, setCurrEvents] = useState<EventBoxProps[]>(Events);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearchQuery = e.target.value;
+    setSearchQuery(newSearchQuery);
+
+    const filteredEvents = Events.filter((event) =>
+      event.EventName.toLowerCase().includes(newSearchQuery.toLowerCase())
+    );
+
+    const finalFilteredEvents =
+      selectedFaculty && selectedFaculty !== "คณะ / Faculty"
+        ? filteredEvents.filter(
+            (event) => event.EngFaculty === Faculties[selectedFaculty]
+          )
+        : filteredEvents;
+
+    setCurrEvents(finalFilteredEvents);
+  };
+
+  const handleFacultyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = e.target.value;
+    setSelectedFaculty(selectedValue);
+
+    // Filter events based on selected faculty
+    const filteredEvents =
+      selectedValue === "คณะ / Faculty"
+        ? Events
+        : Events.filter(
+            (event) => event.EngFaculty === Faculties[selectedValue]
+          );
+
+    const finalFilteredEvents =
+      searchQuery !== ""
+        ? filteredEvents.filter((event) =>
+            event.EventName.toLowerCase().includes(searchQuery.toLowerCase())
+          )
+        : filteredEvents;
+
+    setCurrEvents(finalFilteredEvents);
+  };
+
   return (
-    <div className=" text-white">
-      <p className="text-center text-4xl">Events</p>
-      <p className="text-center text-xl underline">กิจกรรมต่าง ๆ ในงาน</p>
-      <div className="grid grid-cols-3 gap-6">
-        <EventBox />
-        <EventBox />
-        <EventBox />
-        <EventBox />
+    <div className="flex flex-col gap-2 text-center text-white">
+      <p className="text-4xl">Events</p>
+      <p className="text-xl underline">กิจกรรมต่าง ๆ ในงาน</p>
+      <div className="mb-8 flex justify-center">
+        <label
+          className="flex h-full w-12 items-center justify-center rounded-l-2xl bg-white p-2"
+          htmlFor="eventName"
+        >
+          <i className="icon-[mdi--magnify] text-3xl text-pink-500"></i>
+        </label>
+        <input
+          type="search"
+          name="eventName"
+          id="eventName"
+          className="w-96 rounded-r-2xl border-2 border-white bg-transparent px-4 text-white"
+          placeholder="ค้นหา/search"
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        <select
+          name="Faculty"
+          id="Faculty"
+          className="ml-4 w-72 appearance-none rounded-lg px-4 text-pink-500"
+          onChange={handleFacultyChange}
+          value={selectedFaculty || ""}
+        >
+          <option value="คณะ / Faculty">คณะ / Faculty</option>
+          {Object.entries(Faculties).map(([id, name]) => (
+            <option key={id} value={id}>
+              {name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="grid grid-cols-1 gap-6 text-start lg:grid-cols-3">
+        {currEvents.map((d, index) => (
+          <EventBox key={index} {...d} />
+        ))}
       </div>
     </div>
   );
-}
+};
+
+export default EventContainer;
