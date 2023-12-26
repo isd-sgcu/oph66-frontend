544e50dc2c039b7b6ab34d7c2fc43f8b065cbb2a
shalluv <leon555thai@gmail.com>
2023-12-26 07:35:00 +0700

    feat(idcard): add functions and refactor ui

---
+++ src/components/IDCard/QRCode.tsx
@@ -0,0 +1,33 @@
+import QR from "qrcode";
+
+import { useEffect, useRef } from "react";
+interface Props {
+  userId: string;
+}
+
+const QRCode = ({ userId }: Props) => {
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  const generateQRCode = async () => {
+    const canvas = ref.current;
+    if (canvas) {
+      QR.toCanvas(canvas, userId, {
+        errorCorrectionLevel: "H",
+        margin: 1,
+        width: 112,
+        color: {
+          dark: "#0000",
+          light: "#fff",
+        },
+      });
+    }
+  };
+
+  useEffect(() => {
+    generateQRCode();
+  }, []);
+
+  return <canvas ref={ref} className="aspect-square w-28"></canvas>;
+};
+
+export default QRCode;
