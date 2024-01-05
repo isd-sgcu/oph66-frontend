1d4f94008d8a972cbc8e8a26e93c33cbe5b1040f
shalluv <leon555thai@gmail.com>
2024-01-05 15:45:57 +0700

    refactor(faqs): reaforting faqs page and reduce unnecessary interactive components

---
+++ src/components/FAQs/FAQsData.ts
@@ -0,0 +1,122 @@
+import type { Props as TextBoxProps } from "./TextBox.astro";
+
+export const FAQsThai: TextBoxProps[] = [
+  {
+    question: "จุฬาฯ จะจัด Open House วันไหน?",
+    answer:
+      "งาน CU Open House 2024 ของส่วนกลางจะจัดขึ้นในวันที่ 20 - 21 มกราคม 2567 ที่จุฬาลงกรณ์มหาวิทยาลัย (ภายในศาลาพระเกี้ยว) โดยมี 8 คณะ 1 สำนักวิชา 1 สถาบัน จัดงาน Open House ที่คณะ แต่ส่วนกลาง",
+  },
+  {
+    question: "ในงานมีกิจกรรมอะไรบ้าง",
+    answer:
+      "ภายในศาลาพระเกี้ยวจะมีเวทีกลางที่เต็มไปด้วยกิจกรรมต่าง ๆ ไม่ว่าจะเป็นการแสดง กิจกรรม Talk Show โดยมีวิทยากรศิษย์เก่าชื่อดังมาร่วมพูดคุยด้วย นอกจากนี้ยังมีซุ้มกิจกรรมจากทุกคณะที่รอให้ผู้เข้าร่วมงานทุกคนมาเยี่ยมชม สำหรับข้อมูลกิจกรรม OPH ของแต่ละคณะ รอติดตามรายละเอียดเพิ่มเติมจาก IG ของแต่ละคณะ",
+  },
+  {
+    question: "กิจกรรมทั้งสองวันเหมือนกันไหม",
+    answer:
+      "กิจกรรมแนะนำคณะต่าง ๆ จะเหมือนกันทั้ง 2 วัน แต่ในส่วนของกิจกรรมพิเศษบางอย่าง จะแตกต่างกันออกไปในแต่ละวัน ทั้งนี้ สามารถดูเพิ่มเติมได้ในหน้ากิจกรรม",
+  },
+  {
+    question: "จำเป็นต้องไปทั้งสองวันไหม",
+    answer: "สามารถเลือกมาวันใดวันหนึ่งก็ได้ หรือจะเข้ามาทั้งสองวันก็ได้",
+  },
+  {
+    question: "จำกัดระดับชั้นผู้เข้าร่วมงานไหม?",
+    answer:
+      "ไม่จำกัดระดับชั้น สามารถเข้าเยี่ยมชมได้ทุกคน นอกจากนี้ ผู้ปกครองก็สามารถเข้าเยี่ยมชมได้ด้วยนะ",
+  },
+  {
+    question: "เข้าร่วมกิจกรรมแล้วได้เกียรติบัตรไหม?",
+    answer: "กิจกรรมของส่วนกลางไม่มีเกียรติบัตร",
+  },
+  {
+    question: "มีงานในส่วนออนไลน์ไหม",
+    answer:
+      "งานส่วนกลางภายในศาลาพระเกี้ยวจะมีการถ่ายทอดสดเวทีกลางผ่านทางช่องทางประชาสัมพันธ์ออนไลน์ (Facebook)",
+  },
+  {
+    question: "วันงานต้องแต่งตัวยังไง",
+    answer:
+      "สามารถใส่ชุดนักเรียน ชุดสุภาพ หรือชุดที่เหมาะสำหรับทำกิจกรรมต่าง ๆ ภายในงานและ workshop ที่เข้าร่วม",
+  },
+  {
+    question: "จำเป็นต้องลงทะเบียนหรือไม่? ใครต้องลงทะเบียนบ้าง?",
+    answer:
+      "ทุกคนต้องลงทะเบียน ถ้าผู้ปกครองไปกับนักเรียนสามารถใช้ของนักเรียนคนเดียวได้",
+  },
+  {
+    question: "เปิดให้ลงทะเบียนตอนไหน? มีปิดลงทะเบียนไหม?",
+    answer:
+      "จะมีการเริ่มลงทะเบียนผ่านทางเว็บไซต์ได้ตั้งแต่วันที่ 6 มกราคม 2567 เป็นต้นไปจนถึงวันงาน โดยไม่มีการปิดลงทะเบียน",
+  },
+  {
+    question: "สามารถไปลงทะเบียนหน้างานที่ศาลาพระเกี้ยวได้มั้ย?",
+    answer:
+      "เบื้องต้นสามารถลงทะเบียน Walk-In ที่ศาลาพระเกี้ยวได้แต่อย่างไรก็ตามยังคงแนะนำให้มีการลงทะเบียนไปก่อนถึงวันงานเพื่อความสะดวกยิ่งขึ้น",
+  },
+  {
+    question: "ถ้าลงทะเบียนไปแล้ว เราต้องไปทำอะไรหน้างานบ้าง",
+    answer:
+      "เมื่อลงทะเบียนแล้วจะได้รับ QR Code แล้วสามารถนำ QR Code ตัวนี้ไปยื่นได้ทุกคณะ รวมถึงการเข้าร่วมงานของส่วนกลางที่บริเวณศาลาพระเกี้ยวด้วย",
+  },
+];
+export const FAQsEng: TextBoxProps[] = [
+  {
+    question: "When is Chulalongkorn University's Open House scheduled?",
+    answer:
+      "Chulalongkorn University's Open House is set for January 20-21, 2024, at Sala Phra Kiew. Additionally, on the same days, eight faculties and two schools within the university will also be hosting their respective Open House events.",
+  },
+  {
+    question: "What activities can I expect at CU Open House 2024?",
+    answer:
+      "CU Open House 2024, held at Sala Phra Kiew, will feature a diverse range of activities. You can enjoy performances, talk with distinguished CU alumni, and more various interactive events. Moreover, each faculty and school will be showcasing their curriculum. For specific details about each faculty's activities, please refer to their respective Instagram accounts.",
+  },
+  {
+    question: "Are the activities the same on both days?",
+    answer:
+      "There will be a faculty advising session on both days. However, special activities vary on each day. Please check for more information on the activity page.",
+  },
+  {
+    question: "Is it necessary to attend both days?",
+    answer: "You're welcome to join either day, or both if you'd like.",
+  },
+  {
+    question: "Can students from any grade attend Open House?",
+    answer:
+      "Absolutely! Our Open House welcomes students from all grades. Parents are also invited to join the event.",
+  },
+  {
+    question: "Will I receive a certificate for attending CU Open House?",
+    answer: "No, certificates will not be provided for participants.",
+  },
+  {
+    question: "Are there online activities?",
+    answer:
+      "Yes, you can catch a live broadcast from the main stage at Sala Phra Kiew.",
+  },
+  {
+    question: "Is there any dress code?",
+    answer:
+      "No. A uniform is not mandatory. You’re free to wear anything you feel comfortable with, or anything suitable for the event or the workshop you’ve registered for.",
+  },
+  {
+    question: "Is registering necessary? Who needs to register?",
+    answer:
+      "Yes, everyone must register. If a student is accompanied by their parents, only the student needs to register.",
+  },
+  {
+    question: "When will registration be available?",
+    answer:
+      "Registration opens on our main website from January 6, 2024, with no closing date.",
+  },
+  {
+    question: "Is online registration necessary before the Open House day?",
+    answer:
+      "Walk-in registration is available on the event day, but it's highly recommended to complete it ahead of time.",
+  },
+  {
+    question: "I have already registered, what's the next step?",
+    answer:
+      "After registering, each student will receive their own QR code. This QR code serves as a pass for visiting all faculties, schools, and Sala Phra Kiew. Simply show the QR code to the staff at the front table.",
+  },
+];
