7a5fa5d8d7b35596583557ef53aaa3136675ee0c
shalluv <leon555thai@gmail.com>
2024-01-06 16:19:35 +0700

    refactor(faculty): finalize faculty page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  PUBLIC_BACKEND_BASEURL: string;
+  PUBLIC_API_BASE_URL: string;
 }
---
+++ src/types/FAQ.ts
@@ -0,0 +1,4 @@
+export interface FAQ {
+  question: string;
+  answer: string;
+}
