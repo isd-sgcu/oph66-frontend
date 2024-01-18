90001addbf69bd2caf5dbf2e7f13db1f49d34b02
shalluv <leon555thai@gmail.com>
2024-01-19 02:05:25 +0700

    fix(day-left): day left < 0 fixed

--- src/components/Landing/Day.tsx
+++ src/components/Landing/Day.tsx
@@ -49,7 +49,7 @@ const Day = () => {
     <div className="mt-10 h-28 w-44 md:mt-14 md:h-auto md:w-auto">
       <img src={union.src} alt="union" />
       <div className="absolute -translate-y-40 translate-x-10 rotate-2 font-libre text-2xl font-normal md:-translate-y-64 md:translate-x-[5.3rem] md:text-4xl">
-        {result !== 0 ? (
+        {result > 1 ? (
           <div>
             <div className="flex w-44 -translate-x-12 justify-center md:-translate-x-8">
               <Counter from={result + 50} to={result} />
@@ -59,14 +59,14 @@ const Day = () => {
             <h1 className="underline underline-offset-8">LEFT</h1>
           </div>
         ) : (
-          <div className="md:-translate-y-10">
-            <p className="-translate-x-5 text-6xl font-bold md:-translate-x-10 md:text-8xl">
+          <div className="md:-translate-x-4 md:-translate-y-8">
+            <p className="-translate-x-4 font-ibm text-6xl font-bold md:-translate-x-8 md:text-8xl">
               วันนี้
             </p>
             <h1 className="mb-2 -translate-x-2 underline underline-offset-8">
               TODAY
             </h1>
-            <h1 className="-translate-x-6 text-lg md:text-2xl">
+            <h1 className="-translate-x-6 text-base md:text-2xl">
               Come Visit Us!
             </h1>
           </div>
