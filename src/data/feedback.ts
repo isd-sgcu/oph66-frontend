e0759785622efa48d616902234b683d075dad81b
shalluv <leon555thai@gmail.com>
2024-01-19 02:06:17 +0700

    feat(feedback): add feedback form for the event

---
+++ src/data/feedback.ts
@@ -0,0 +1,99 @@
+import type { Question } from "@/types/feedback";
+
+export const QUESTIONS: Question[] = [
+  {
+    order: 1,
+    th: "1. ความสะดวกในการเดินทางขณะร่วมงาน (รถปอ.พ. การจราจรภายในมหาวิทยาลัย)",
+    en: "1. Convenience of transportation during the event (POP buses, university traffic)",
+  },
+  {
+    order: 2,
+    th: "2. ความชัดเจนและเข้าใจง่ายของแผนที่",
+    en: "2. Clarity and ease of understanding of the map",
+  },
+  {
+    order: 3,
+    th: "3. ความเพรียบพร้อมของข้อมูลต่าง ๆ ในเว็บไซต์",
+    en: "3. Clarity and completeness of information on the website",
+  },
+  {
+    order: 4,
+    th: "4. ความเป็นระเบียบเรียบร้อยในการจัดการจราจรภายในงาน",
+    en: "4. Orderliness of traffic management within the event",
+  },
+  {
+    order: 5,
+    th: "5. ความทั่วถึงและเพียงพอในการประชาสัมพันธ์เกี่ยวกับงาน",
+    en: "5. Adequacy and sufficiency of publicity about the event",
+  },
+  {
+    order: 6,
+    th: "6. การอำนวยความสะดวกและการให้ข้อมูลแก่ผู้ร่วมงานของฝ่าย Information",
+    en: "6. Convenience and information provided to participants by the Information Department",
+  },
+  {
+    order: 7,
+    th: "1. ความเหมาะสมของระยะเวลาในการจัดกิจกรรมเวทีกลาง",
+    en: "1. Appropriateness of the duration of activities on the main stage",
+  },
+  {
+    order: 8,
+    th: "2. ความเหมาะสมของรูปแบบของกิจกรรมเวทีกลาง",
+    en: "2. Suitability of the format for activities on the main stage",
+  },
+  {
+    order: 9,
+    th: "3. ความเหมาะสมและเพียงพอของสถานที่จัดงาน",
+    en: "3. Suitability and adequacy of the event venue",
+  },
+  {
+    order: 10,
+    th: "4. ความน่าสนใจและดึงดูดของกิจกรรมเวทีกลาง",
+    en: "4. Interest and attractiveness of activities on the main stage",
+  },
+  {
+    order: 11,
+    th: "5. ความรู้และประโยชน์ที่ได้รับจากวิทยากรศิษย์เก่าของจุฬาลงกรณ์มหาวิทยาลัย",
+    en: "5. Knowledge and benefits gained from alumni speakers of Chulalongkorn University",
+  },
+  {
+    order: 12,
+    th: "6. ความรู้และประโยชน์ที่ได้รับจากวิทยากรศิษย์ปัจจุบันของจุฬาลงกรณ์มหาวิทยาลัย",
+    en: "6. Knowledge and benefits gained from current student speakers of Chulalongkorn University",
+  },
+  {
+    order: 13,
+    th: "7. ความรู้และประโยชน์ที่ได้รับจากกิจกรรมต่าง ๆ ในบูธคณะ",
+    en: "7. Knowledge and benefits gained from activities in faculty booths",
+  },
+  {
+    order: 14,
+    th: "8. การอำนวยความสะดวกและการช่วยเหลือของสตาฟ",
+    en: "8. Convenience and assistance from staff",
+  },
+  {
+    order: 15,
+    th: "9. ภาพรวมการตกแต่งภายในศาลาพระเกี้ยว",
+    en: "9. Overall interior decoration of Sala Phra Kiew",
+  },
+  {
+    order: 16,
+    th: "10. ความสร้างสรรค์ของกิจกรรมภายในบูธกิจกรรมกลาง",
+    en: "10. Creativity of activities within Sala Phra Kiew activity booths",
+  },
+  {
+    order: 17,
+    th: "11. ความสนุกสนานและความเหมาะสมของบูธกิจกรรมกลางภายในศาลาพระเกี้ยว",
+    en: "11. Fun and appropriateness of activities within Sala Phra Kiew activity booths ",
+  },
+  {
+    order: 18,
+    th: "12. ประโยชน์ที่ได้รับจากการบรรยายแนะนำภายในบูธกิจกรรมกลาง",
+    en: "12. Benefits derived from the advice session within Sala Phra Kiew activity booths",
+  },
+  {
+    order: 19,
+    th: "13. บูธกิจกรรมกลางตอบโจทย์กับผู้เข้าร่วมงานมากเพียงใด",
+    en: "13. To what extent did the Sala Phra Kiew activity booths meet participants' expectations",
+  },
+];
